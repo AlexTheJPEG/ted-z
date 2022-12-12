7230390b9fab6a4c53eb363874fd003067d59250
Alex The JPEG <alexthejpeg@gmail.com>
2022-12-11 23:28:31 -0500

    Add fun plugin

---
+++ ted-z/plugins/fun.py
@@ -0,0 +1,62 @@
+import random
+from typing import Annotated as Atd
+
+import crescent
+import hikari
+
+from util.helper import get_dir_path
+
+plugin = crescent.Plugin()
+
+with open(f"{get_dir_path(__file__)}/files/slaps.txt", 'r') as file:
+    SLAPS = [line.strip() for line in file.readlines()]
+
+
+@plugin.include
+@crescent.command(name="slap", description="Slap someone")
+async def slap(ctx: crescent.Context, user: Atd[hikari.User, "The person you want to slap"]) -> None:
+    response = random.choice(SLAPS)
+
+    if ctx.user.username == user.username:
+        slapee = "yourself"
+        slap_message = response.format(slapee)
+        slap_message = slap_message.replace(" they", " you")
+        slap_message = slap_message.replace(" their", " your")
+        slap_message = slap_message.replace(" them", " yourself")
+    elif f"{user.username}#{user.discriminator}" == "Ted#2395":
+        slapee = "me"
+        slap_message = response.format(slapee)
+        slap_message = slap_message.replace(" they", " I")
+        slap_message = slap_message.replace(" their", " my")
+        slap_message = slap_message.replace(" them", " myself")
+    else:
+        slapee = user.username
+        slap_message = response.format(slapee)
+
+    await ctx.respond(slap_message)
+
+
+def create_ground_string(groundee, reason):
+    time = random.randrange(10 ** 50, 10 ** 51)
+    oh = "OH" * random.randint(15, 30)
+    grounded = ("GROUNDED " * random.randint(3, 9)).strip()
+    time_unit = random.choice(["YEARS", "CENTURIES", "EONS", "ETERNITIES"])
+    return (
+        f"{oh} {groundee.upper()} HOW DARE YOU {reason.upper()}!!! "
+        f"THAT'S IT. YOU ARE {grounded} FOR {time} {time_unit}!!!!!!!!!!"
+    )
+
+
+@plugin.include
+@crescent.command(name="slap", description="Ground someone")
+async def ground(ctx: crescent.Context,
+                 user: Atd[hikari.User, "The person you want to ground"],
+                 reason: Atd[str, "The reason you're grounding them"]) -> None:
+    ground_message = create_ground_string(user.username, reason)
+    await ctx.respond(ground_message)
+
+
+@plugin.include
+@crescent.command(name="thegame", description="You lost The Game. Now make everyone else lose it")
+async def thegame(ctx: crescent.Context) -> None:
+    await ctx.respond("I lost The Game.")
