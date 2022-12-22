33589157452d0ff3ac3ef76bd50f892d5e20475d
Alex The JPEG <alexthejpeg@gmail.com>
2022-12-21 23:25:08 -0500

    Move Bot class to main

--- ted-z/__main__.py
+++ ted-z/__main__.py
@@ -1,9 +1,41 @@
-from bot import Bot
+from abc import ABC
+from typing import Any
+
+import aiohttp
+import crescent
+import hikari
+import tomli
+
+with open("bot_settings.toml", mode="rb") as fp:
+    config = tomli.load(fp)
 
 
-def main() -> None:
-    Bot().run()
+class Bot(crescent.Bot, ABC):
+    def __init__(self) -> None:
+        super().__init__(
+            token=config["bot"]["token"],
+            intents=hikari.Intents.ALL_UNPRIVILEGED
+        )
+
+        self.plugins.load_folder("ted-z.plugins")
+
+        self._session: aiohttp.ClientSession | None = None
+
+    def session(self) -> aiohttp.ClientSession:
+        if not self._session:
+            raise AttributeError("Session has not been set yet.")
+        return self._session
+
+    async def start(self, *args: Any, **kwargs: Any) -> None:
+        self._session = aiohttp.ClientSession()
+        await super().start(*args, **kwargs)
+
+    async def join(self) -> None:
+        await super().join()
+        if self._session and not self._session.closed:
+            await self._session.close()
 
 
 if __name__ == "__main__":
-    main()
+    bot = Bot()
+    bot.run()
