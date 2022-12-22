465aa806f49ddd7830b1d621d0662fcf0f09351d
Alex The JPEG <alexthejpeg@gmail.com>
2022-12-22 17:14:49 -0500

    Switch to argostranslate, offline library

--- ted-z/plugins/translate.py
+++ ted-z/plugins/translate.py
@@ -1,15 +1,23 @@
-import os
 import json
-import random
+import os
 
+import argostranslate.translate
 import crescent
 
-from goslate import Goslate
+real_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(real_path)
+with open(f"{dir_path}/files/langcodes.json", 'r') as file:
+    LANGUAGES = json.load(file)
 
-TRANSLATOR = Goslate()
-LANGUAGES = TRANSLATOR.get_languages()
 LANGUAGE_CODES = list(LANGUAGES.keys())
 LANGUAGE_CHOICES = [f"{k}: {v.title()}\n" for k, v in LANGUAGES.items()]
+
+# Uncomment these lines to install all the translation language models
+# import argostranslate.package
+# argostranslate.package.update_package_index()
+# available_packages = argostranslate.package.get_available_packages()
+# for package in available_packages:
+#     argostranslate.package.install_from_path(package.download())
 
 plugin = crescent.Plugin()
 
@@ -20,29 +28,33 @@ class LangCodesCommand:
     async def callback(self, ctx: crescent.Context) -> None:
         # TODO: Make this a paginated embed
         await ctx.respond("Here is a list of every language code you can use with translate commands,"
-                        "as well as the languages they correspond to:\n\n"
-                        f"{''.join(LANGUAGE_CHOICES)}", ephemeral=True)
+                          "as well as the languages they correspond to:\n\n"
+                         f"{''.join(LANGUAGE_CHOICES)}", ephemeral=True)
 
 
 @plugin.include
 @crescent.command(name="translate", description="Translate a phrase from one language to another")
 class TranslateCommand:
-    source = crescent.option(str, "The source language (use \"detect\" to autodetect the language)")
+    source = crescent.option(str, "The source language") # (use \"detect\" to autodetect the language)")
     destination = crescent.option(str, "The language to translate to")
     phrase = crescent.option(str, "The phrase to translate")
 
     async def callback(self, ctx: crescent.Context) -> None:
         match (self.source, self.destination):
             case ("detect", dest) if dest in LANGUAGE_CODES:
-                await ctx.respond(f"💬 **{self.phrase}** translated to {LANGUAGES[dest].title()}:"
-                                f"\n\n{TRANSLATOR.translate(text=self.phrase, target_language=dest)}")
+                # TODO: Add language detection; this library doesn't seem to have it
+                await ctx.respond("Sorry, language detection isn't a thing yet; try again later!")
             case (src, dest) if src in LANGUAGE_CODES and dest in LANGUAGE_CODES:
+                translation = argostranslate.translate.translate(self.phrase, src, dest)
                 await ctx.respond(f"💬 **{self.phrase}** translated from {LANGUAGES[src].title()} to {LANGUAGES[dest].title()}:"
-                                f"\n\n{TRANSLATOR.translate(text=self.phrase, target_language=dest, source_language=src)}")
-            case _:
-                await ctx.respond(f"Either the source ({self.source}) or the destination ({self.destination}) language (or both) is invalid!"
-                                  "\n\nUse `/langcodes` to see what languages you can use. "
-                                  "You can also use \"detect\" as the source language to autodetect the language.")
+                                f"\n\n{translation}")
+            case (src, _) if src not in LANGUAGE_CODES:
+                await ctx.respond(f"The source ({src}) language is either invalid or unsupported!"
+                                  "\n\nUse `/langcodes` to see what languages you can use.")
+            case (_, dest) if dest not in LANGUAGE_CODES:
+                await ctx.respond(f"The destination ({dest}) language is either invalid or unsupported!"
+                                  "\n\nUse `/langcodes` to see what languages you can use.")
+                                  # "You can also use \"detect\" as the source language to autodetect the language.")
 
 
 # TODO: Bad translate command
