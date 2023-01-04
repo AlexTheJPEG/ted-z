bd804577892c90382b587334f7ae1d7e3c3a55d4
Alex The JPEG <alexthejpeg@gmail.com>
2023-01-03 23:42:39 -0500

    Prepare moving to lightbulb

--- ted-z/__main__.py
+++ ted-z/__main__.py
@@ -1,6 +1,16 @@
-from .bot import Bot
+import lightbulb
+from hikari import Intents
 
+from .utils.opening import load_bot_settings
+
+CONFIG = load_bot_settings()
+
+INTENTS = Intents.ALL_UNPRIVILEGED
+
+bot = lightbulb.BotApp(
+    CONFIG["bot"]["token"],
+    intents=INTENTS,
+)
 
 if __name__ == "__main__":
-    bot = Bot()
     bot.run()
