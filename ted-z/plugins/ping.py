173814f85095ad43ff645acc9c486252772bb54d
Alex The JPEG <alexthejpeg@gmail.com>
2023-01-04 14:04:35 -0500

    Clarify name and description fields

--- ted-z/plugins/ping.py
+++ ted-z/plugins/ping.py
@@ -4,7 +4,7 @@ plugin = lightbulb.Plugin("ping")
 
 
 @plugin.command
-@lightbulb.command("ping", "Ping Ted")
+@lightbulb.command(name="ping", description="Ping Ted")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def ping(ctx: lightbulb.Context) -> None:
     await ctx.respond(f":ping_pong: Pong! ({round(ctx.app.heartbeat_latency * 1000)} ms)")
